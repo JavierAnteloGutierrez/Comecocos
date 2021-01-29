980ee513d082cebb61aa745078d55f36925c6769
DavidRecuero <david.recuero.solis@alumnojoyfe.iepgroup.es>
2021-01-30 00:44:13 +0100

    PACMAN

--- Comecocos/MainWindow.xaml.vb
+++ Comecocos/MainWindow.xaml.vb
@@ -1,3 +1,9 @@
 ﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
 
+    End Sub
+
+    Private Sub OpenWindow(sender As Object, e As RoutedEventArgs)
+        Window1 = New Window1();
+    End Sub
 End Class
